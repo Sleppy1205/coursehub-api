264a5e5b26e2a16bcd01115a2bd3b8ccf4ea9990
Ricardo Delgado <ricardojhamil06@gmail.com>
2026-09-14 15:01:25 -0500

    avance

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CoursesService } from './courses.service.js';
+import { CoursesController } from './courses.controller.js';
 
-@Module({})
+@Module({
+  providers: [CoursesService],
+  controllers: [CoursesController]
+})
 export class CoursesModule {}
