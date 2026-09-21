56515187d2d9f5ca80816d0dd9df566bc8f58dbf
Ricardo Delgado <ricardojhamil06@gmail.com>
2026-09-21 13:34:56 -0500

    refactorizacion para exportar StudentsService y CoursesService

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -4,6 +4,7 @@ import { CoursesController } from './courses.controller.js';
 
 @Module({
   providers: [CoursesService],
-  controllers: [CoursesController]
+  controllers: [CoursesController],
+  exports: [CoursesService],
 })
 export class CoursesModule {}
